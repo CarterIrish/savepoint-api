84d845f6914f115b243f0d05572fb422fe2a5a29
Carter Irish <2023c.irish@gmail.com>
2026-02-21 19:50:28 -0500

    Added method-based routing and scaffold API endpoints
    - Stub out all planned endpoints (/api/games/:idOrSlug, /api/genres, /api/platforms) with correct handler signatures and WIP responses.
    
    - Added a styleText import from node:util to enhance console debugging

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,36 +1,53 @@
 const database = require('../db/games.json');
 
 const sendResponse = (res, status, obj, method) => {
-      const body = JSON.stringify(obj);
-      res.writeHead(status, {
-          "Content-Type": "application/json",
-          "Content-Length": Buffer.byteLength(body),
-      });
-      if (method !== "HEAD" && status !== 204) { res.write(body); }
-      res.end();
+    const body = JSON.stringify(obj);
+    res.writeHead(status, {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(body),
+    });
+    if (method !== "HEAD" && status !== 204) { res.write(body); }
+    res.end();
 }
 
-const NotFound = (request, response) => {
-    sendResponse(response, 404, { message: "Not Found" }, request.method);
+const notFound = (request, response) => {
+    sendResponse(response, 404, { message: "The endpoint you are looking for was not found", id:'notFound' }, request.method);
 }
 
-const GetGames = (request, response, parsedUrl) =>
-{
+const getGames = (request, response, parsedUrl) => {
     const params = parsedUrl.searchParams;
 
     let games = database.games.map(({
-        id, name, slug, cover, genres, platforms, rating, rating_count, first_release_date
+        id, name, slug, url, cover, genres, platforms, rating, rating_count, first_release_date
     }) => ({
-        id, name, slug, cover, genres, platforms, rating, rating_count, first_release_date
+        id, name, slug, url, cover, genres, platforms, rating, rating_count, first_release_date
     }));
-
     sendResponse(response, 200, { games }, request.method);
 }
 
-const AddGames = (request, response, parsedUrl) =>
-{
+const createGame = (request, response, parsedUrl) => {
     const params = parsedUrl.searchParams;
-    sendResponse(response, 404, {message: 'Work In Progress'}, request.method);
+    sendResponse(response, 404, { message: 'Work In Progress', params: Object.fromEntries(params) }, request.method);
 }
 
-module.exports = { NotFound , GetGames , AddGames};
+const getGame = (request, response, parsedUrl) => {
+    const parts = parsedUrl.pathname.split('/');
+     const idOrSlug = parts[3];
+    sendResponse(response, 404, { message: 'Work In Progress', idOrSlug }, request.method);
+}
+
+const getGenres = (request, response) => {
+    sendResponse(response, 404, { message: 'Work in progress' }, request.method);
+}
+
+const getPlatforms = (request, response) => {
+    sendResponse(response, 404, { message: 'Work in progress' }, request.method);
+}
+
+const updateGame = (request, response, parsedUrl) => {
+    const parts = parsedUrl.pathname.split('/');
+     const idOrSlug = parts[3];
+    sendResponse(response, 404, { message: 'Work In Progress', idOrSlug }, request.method);
+}
+
+module.exports = { notFound, getGames, createGame, getGame, getGenres, getPlatforms, updateGame };
--- src/server.js
+++ src/server.js
@@ -2,28 +2,39 @@ const http = require('http');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const htmlHandle = require('./htmlResponses');
 const jsonHandle = require('./jsonResponses');
-
+const { styleText } = require('node:util');
 const URL_STRUCT = {
-    '/': {GET:htmlHandle.GetIndex},
-    '/docs': {GET:htmlHandle.GetDocs},
-    '/mainStyle.css': {GET:htmlHandle.GetCss},
-    '/api/notFound': {GET:jsonHandle.NotFound},
-    '/api/games': {GET:jsonHandle.GetGames, POST:jsonHandle.AddGames}
+    '/': { GET: htmlHandle.GetIndex },
+    '/docs': { GET: htmlHandle.GetDocs },
+    '/mainStyle.css': { GET: htmlHandle.GetCss },
+    '/api/notFound': { GET: jsonHandle.notFound },
+    '/api/games': { GET: jsonHandle.getGames, POST: jsonHandle.createGame },
+    '/api/genres': { GET: jsonHandle.getGenres },
+    '/api/platforms': { GET: jsonHandle.getPlatforms },
 }
 
-const onRequest = (request, response) =>
-{
-    console.log(request.url);
+const onRequest = (request, response) => {
+    console.log(`${styleText('cyan', request.method)} ${request.url}`);
 
-    const protocol = request.connection.encrypted ? 'https':'http';
+    const protocol = request.connection.encrypted ? 'https' : 'http';
     const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    const methodMap = URL_STRUCT[parsedURL.pathname];
-    if(!methodMap){jsonHandle.NotFound(request, response); return;}
+    let methodMap = URL_STRUCT[parsedURL.pathname];
+    // No exact match means either 404 or dynamic endpoint ':idOrSlug'
+    if (!methodMap) {
+        const parts = parsedURL.pathname.split('/');
+        if (parts.length === 4 && parts[1] === 'api' && parts[2] === 'games' && parts[3]) {
+            methodMap = { GET: jsonHandle.getGame, POST: jsonHandle.updateGame };
+        }
+        else {
+            jsonHandle.notFound(request, response);
+            return;
+        }
+    }
 
     const method = request.method === 'HEAD' ? 'GET' : request.method;
-    const handler = methodMap[method] || jsonHandle.NotFound;
-    handler(request,response,parsedURL);
+    const handler = methodMap[method] || jsonHandle.notFound;
+    handler(request, response, parsedURL);
 }
 
 http.createServer(onRequest).listen(port, () => {
